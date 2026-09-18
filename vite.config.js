b00f9ac50b4903ff8116fddf14d114280ac62a2e
Tejasv Sahu <tejasvsahu4@gmail.com>
2026-09-18 17:27:39 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
-// Replace YOUR_REPO_NAME with the exact name of your GitHub repository.
 export default defineConfig({
-  base: '/YOUR_REPO_NAME/',
+  base: '/',
 })
